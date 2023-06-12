f933ae1637a2ed948fb5199a54fa26e547552c2b
Yugandhar182 <yugandhar@recruitly.io>
2023-06-12 15:03:01 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
   let gridData = [];
  
     const fileButtonTemplate = (container, options) => {
-      const candidateId = options.data.id;
+      const candidateId = options.data.key.id;
     const button = document.createElement("button");
    button.className = "btn btn-primary btn-sm";
      button.innerText = "Upload CV";
