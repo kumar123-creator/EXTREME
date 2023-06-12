a480d1fb61a641313c81b23715953c1f53a7afdc
Yugandhar182 <yugandhar@recruitly.io>
2023-06-12 13:18:59 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -17,14 +17,14 @@
 
     input.addEventListener("change", async (e) => {
       const file = e.target.files[0];
-      console.log("Data being sent to API:", e.options.data.id);
+      console.log("Data being sent to API:", e.options.data);
       if (file) {
         try {
           const formData = new FormData();
           formData.append("cv", file);
 
           const response = await fetch(
-            `https://api.recruitly.io/api/candidate/${options.data.id}/cv?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`,
+            `https://api.recruitly.io/api/candidate/${e.options.data.id}/cv?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`,
             {
               method: "POST",
               body: formData,
