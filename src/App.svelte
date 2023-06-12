e1177926520f7a27901a00e5c4090b3aaba94f82
Yugandhar182 <yugandhar@recruitly.io>
2023-06-12 16:39:45 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -6,9 +6,9 @@
   let jsonData = [];
   let gridData = [];
   let isCVUploadPopupVisible = false;
+  let isViewCvPopupVisible = false;
   let selectedRowData = null;
-  let isViewCVPopupVisible = false;
-  let cvImageUrl = "";
+  let viewCvUrl = null;
 
   async function uploadCV(file) {
     // Perform further actions with the uploaded file
@@ -92,9 +92,9 @@
     isCVUploadPopupVisible = false;
   }
 
-  function handleViewCVPopupClose() {
-    isViewCVPopupVisible = false;
-    cvImageUrl = "";
+  function handleViewCvClose() {
+    // Close the CV view popup
+    isViewCvPopupVisible = false;
   }
 
   const dispatch = createEventDispatcher();
@@ -107,224 +107,117 @@
     jsonData = responseData.data;
 
     gridData = jsonData.map((item) => ({
-      id: item.id,
-      firstName: item.firstName,
-      surname: item.surname,
-      email: item.email,
-      mobile: item.mobile,
-      cvUrl: item.cvUrl, // assuming cvUrl is the property containing the CV file URL
+      ...item,
+      actions: {
+        viewCVButton: {
+          text: "View CV",
+          onClick: function (e) {
+            const rowData = e.data;
+            selectedRowData = rowData;
+            isViewCvPopupVisible = true;
+            viewCvUrl = rowData.cvUrl;
+          },
+        },
+        uploadCVButton: {
+          text: "Upload CV",
+          onClick: function (e) {
+            const rowData = e.data;
+            selectedRowData = rowData;
+            isCVUploadPopupVisible = true;
+          },
+        },
+      },
     }));
 
-    const dataGrid = new DevExpress.ui.dxDataGrid(
-      document.getElementById("dataGrid"),
-      {
-        dataSource: gridData,
-        columns: [
-          { dataField: "id", caption: "ID", width: 250 },
-          { dataField: "firstName", caption: "First Name", width: 180 },
-          { dataField: "surname", caption: "Surname", width: 180 },
-          { dataField: "email", caption: "Email", width: 180 },
-          { dataField: "mobile", caption: "Mobile", width: 100 },
-          {
-            caption: "Actions",
-            width: 350,
-            cellTemplate: function (container, options) {
-              const cvUploadButton = document.createElement("button");
-              cvUploadButton.innerText = "CV Upload";
-              cvUploadButton.classList.add("btn", "btn-primary", "mr-2");
-              cvUploadButton.addEventListener("click", function () {
-                const rowData = options.data;
-                selectedRowData = rowData;
-                isCVUploadPopupVisible = true;
-              });
-
-              const cvDownloadButton = document.createElement("button");
-              cvDownloadButton.innerText = "CV Download";
-              cvDownloadButton.classList.add("btn", "btn-info", "mr-2");
-              cvDownloadButton.addEventListener("click", function () {
-                const rowData = options.data;
-                const cvUrl = rowData.cvUrl; // Assuming cvUrl is the property containing the CV file URL
-                downloadCV(cvUrl);
-              });
-
-              const viewCVButton = document.createElement("button");
-              viewCVButton.innerText = "View CV";
-              viewCVButton.classList.add("btn", "btn-secondary");
-              viewCVButton.addEventListener("click", function () {
-                const rowData = options.data;
-                const cvUrl = rowData.cvUrl; // Assuming cvUrl is the property containing the CV file URL
-                cvImageUrl = cvUrl;
-                isViewCVPopupVisible = true;
-              });
-
-              container.appendChild(cvUploadButton);
-              container.appendChild(cvDownloadButton);
-              container.appendChild(viewCVButton);
-            },
-          },
-        ],
-        showBorders: true,
-        filterRow: {
-          visible: true,
-        },
-        editing: {
-          allowDeleting: true,
-          allowAdding: true,
-          allowUpdating: true,
-          mode: "popup",
-          form: {
-            labelLocation: "top",
-          },
-          popup: {
-            showTitle: true,
-            title: "Row in the editing state",
-          },
-          texts: {
-            saveRowChanges: "Save",
-            cancelRowChanges: "Cancel",
-            deleteRow: "Delete",
-            confirmDeleteMessage: "Are you sure you want to delete this record?",
-          },
-          onSaveRowChanges: handleSave, // Bind handleSave function to the saveRowChanges event
-        },
-        paging: {
-          pageSize: 10,
-        },
-        onRowInserting: async (e) => {
-          console.log("Data being sent to API:", e.data);
-          try {
-            const response = await fetch(
-              "https://api.recruitly.io/api/candidate?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA",
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(e.data),
-              }
-            );
-
-            const responseData = await response.json();
-            if (response.ok) {
-              e.data.firstName = responseData.firstName;
-              gridData.push(e.data);
-              dataGrid.refresh();
-            } else {
-              console.error("Failed to add record:", responseData.error);
-            }
-          } catch (error) {
-            console.error("Failed to add record:", error);
-          }
-        },
-        onRowUpdating: async (e) => {
-          try {
-            console.log(e);
-            var newData = {
-              id: e.key.id,
-              firstName:
-                e.newData.firstName === undefined
-                  ? e.oldData.firstName
-                  : e.newData.firstName,
-              surname:
-                e.newData.surname === undefined ? e.oldData.surname : e.newData.surname,
-              email: e.newData.email === undefined ? e.oldData.email : e.newData.email,
-              mobile:
-                e.newData.mobile === undefined ? e.oldData.mobile : e.newData.mobile,
-            };
-
-            console.log(newData);
-            const response = await fetch(
-              `https://api.recruitly.io/api/candidate?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`,
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(newData),
-              }
-            );
-            const responseData = await response.json();
-            if (response.ok) {
-              e.newData = responseData;
-            } else {
-              console.error("Failed to update record:", responseData.error);
-            }
-          } catch (error) {
-            console.error("Failed to update record:", error);
-          }
-        },
-        onRowRemoving: async (e) => {
-          try {
-            const response = await fetch(
-              `https://api.recruitly.io/api/candidate?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA&id=${e.data.id}`,
-              {
-                method: "DELETE",
-              }
-            );
-            if (response.ok) {
-              const index = gridData.findIndex((item) => item.id === e.data.id);
-              gridData.splice(index, 1);
-              dataGrid.refresh();
-            } else {
-              console.error("Failed to delete record:", response.error);
-            }
-          } catch (error) {
-            console.error("Failed to delete record:", error);
-          }
-        },
-      }
-    );
-
-    dataGrid.render();
+    DevExpress.ui.notify("Data loaded successfully", "success", 1000);
   });
 </script>
 
-<style>
-  #dataGrid {
-    height: 400px;
-  }
+<main>
+  <h1>Candidate Grid</h1>
+  <DevExpress.DataGrid
+    dataSource={gridData}
+    allowColumnReordering={true}
+    showBorders={true}
+  >
+    <DevExpress.Column dataField="id" caption="ID"></DevExpress.Column>
+    <DevExpress.Column dataField="name" caption="Name"></DevExpress.Column>
+    <DevExpress.Column
+      dataField="actions.viewCVButton"
+      caption="View CV"
+      cellTemplate={(container, options) => {
+        const viewCVButton = document.createElement("button");
+        viewCVButton.innerText = "View CV";
+        viewCVButton.classList.add("btn", "btn-secondary");
+        viewCVButton.addEventListener("click", function () {
+          const rowData = options.data;
+          selectedRowData = rowData;
+          isViewCvPopupVisible = true;
+          viewCvUrl = rowData.cvUrl;
+        });
 
+        container.appendChild(viewCVButton);
+      }}
+    ></DevExpress.Column>
+    <DevExpress.Column
+      dataField="actions.uploadCVButton"
+      caption="Upload CV"
+      cellTemplate={(container, options) => {
+        const uploadCVButton = document.createElement("button");
+        uploadCVButton.innerText = "Upload CV";
+        uploadCVButton.classList.add("btn", "btn-primary");
+        uploadCVButton.addEventListener("click", function () {
+          const rowData = options.data;
+          selectedRowData = rowData;
+          isCVUploadPopupVisible = true;
+        });
+
+        container.appendChild(uploadCVButton);
+      }}
+    ></DevExpress.Column>
+  </DevExpress.DataGrid>
+
+  {#if isCVUploadPopupVisible}
+    <div class="popup-overlay">
+      <div class="popup-content">
+        <h2>Upload CV</h2>
+        <input type="file" on:change={event => uploadCV(event.target.files[0])} />
+        <button on:click={handleSave} class="btn btn-primary">Save</button>
+        <button on:click={handleClose} class="btn btn-secondary">Close</button>
+      </div>
+    </div>
+  {/if}
+
+  {#if isViewCvPopupVisible}
+    <div class="popup-overlay">
+      <div class="popup-content">
+        <h2>View CV</h2>
+        <img src={viewCvUrl} alt="CV" />
+        <button on:click={handleViewCvClose} class="btn btn-secondary">Close</button>
+      </div>
+    </div>
+  {/if}
+</main>
+
+<style>
   .popup-overlay {
-    display: flex;
-    justify-content: center;
-    align-items: center;
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-color: rgba(0, 0, 0, 0.5);
-    z-index: 9999;
   }
 
   .popup-content {
-    background-color: #fff;
+    background-color: white;
     padding: 20px;
-    border-radius: 4px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 </style>
-
-<h1>Recruitly</h1>
-
-<div id="dataGrid"></div>
-
-{#if isCVUploadPopupVisible}
-<div class="popup-overlay">
-  <div class="popup-content">
-    <h3>CV Upload</h3>
-    <input type="file" accept=".pdf,.doc,.docx,.png,.jpg,.jpeg" on:change="{(e) => uploadCV(e.target.files[0])}">
-    <button class="btn btn-primary" on:click="{handleClose}">Close</button>
-  </div>
-</div>
-{/if}
-
-{#if isViewCVPopupVisible}
-<div class="popup-overlay">
-  <div class="popup-content">
-    <h3>View CV</h3>
-    <img src="{cvImageUrl}" alt="CV Image" on:error="{handleViewCVPopupClose}" />
-    <button id="viewCVPopupCloseButton" on:click="{handleViewCVPopupClose}" class="btn btn-secondary">Close</button>
-  </div>
-</div>
-{/if}
