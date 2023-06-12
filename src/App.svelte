0babce3bceade35a9db120e075b87199a2d24a64
Yugandhar182 <yugandhar@recruitly.io>
2023-06-12 18:21:44 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -86,7 +86,7 @@
   async function downloadCV(cvUrl) {
   try {
     const response = await fetch(
-      `https://api.recruitly.io/api/cloudfile/download?cloudFileId=${cvUrl}&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77`
+      `https://api.recruitly.io/api/cloudfile/download?cloudFileId=${cvUrl}&apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
     );
 
     if (response.ok) {
